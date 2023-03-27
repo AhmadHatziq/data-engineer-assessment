43bdf86d0322ea3ee0141fd287518a596f771a41
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2023-03-27 22:47:18 +0800

    Create table

--- etl.py
+++ etl.py
@@ -9,3 +9,31 @@ conn = psycopg2.connect("host=127.0.0.1 dbname=postgres user=postgres password=p
 conn.set_session(autocommit=True)
 cur = conn.cursor()
 conn.close()
+
+# Create table. 
+create_table_command = """
+CREATE TABLE IF NOT EXISTS msft_stock (
+    id SERIAL PRIMARY KEY, 
+    stock_symbol VARCHAR(10) NOT NULL, 
+    data DATE NOT NULL, 
+    open FLOAT NOT NULL, 
+    high FLOAT NOT NULL, 
+    low FLOAT NOT NULL, 
+    close FLOAT NOT NULL, 
+    adj_close FLOAT NOT NULL, 
+    volume BIGINT NOT NULL
+                    ); 
+"""
+cur.execute(create_table_command)
+
+
+# Read in csv file
+'''
+market_df = pd.read_csv('MSFT.csv') 
+for index, row in market_df.iterrows(): 
+    row_open = row['Opemn']'''
+    
+
+
+
+conn.close()
